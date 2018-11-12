d29b0717385777f2481be17a2fff7554090f1271
spences10 <spences10apps@gmail.com>
2018-11-12 20:57:43 +0000

    bring in context providers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  GalleryProvider,
+  GalleryContext
+} from './contexts/GalleryContext'
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <GalleryProvider>
+    <GalleryContext.Consumer>
+      {({ fetchImages }) => <App fetchImages={fetchImages} />}
+    </GalleryContext.Consumer>
+  </GalleryProvider>,
+  rootElement
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
