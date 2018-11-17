003887e2e9177144677fc2d4c54d3aeef7e73115
spences10 <spences10apps@gmail.com>
2018-11-17 16:53:54 +0000

    add fonts

--- src/components/theme/globalStyle.js
+++ src/components/theme/globalStyle.js
@@ -7,8 +7,8 @@ export const themes = {
     background: '#f9f9fd',
     foreground: '#fefefe',
     border: '#044040',
-    fontHeader: 'Dosis, sans-serif',
-    fontBody: 'Poppins, sans, sans-serif',
+    fontHeader: 'Dancing+Script, sans-serif',
+    fontBody: 'Glegoo, sans, sans-serif',
     fontDark: '#34434b',
     fontLight: '#586368',
     fontWhite: '#ffffff',
@@ -23,8 +23,8 @@ export const themes = {
     background: '#e6e6e6',
     foreground: '#f7f0f0',
     border: '#054545',
-    fontHeader: 'Poppins, sans, sans-serif',
-    fontBody: 'Dosis, sans-serif',
+    fontHeader: 'Lustria, sans, sans-serif',
+    fontBody: 'Stint+Ultra+Condensed, sans-serif',
     fontDark: '#034544',
     fontLight: '#596869',
     fontWhite: '#ffffff',
@@ -39,8 +39,8 @@ export const themes = {
     background: '#f3eff5',
     foreground: '#88d4d8',
     border: '#055455',
-    fontHeader: 'Eczar, sans, sans-serif',
-    fontBody: 'Trirong, sans-serif',
+    fontHeader: 'Work+Sans, sans, sans-serif',
+    fontBody: 'Metrophobic, sans-serif',
     fontDark: '#0d0a0b',
     fontLight: '#454955',
     fontWhite: '#ffffff',
@@ -48,3 +48,18 @@ export const themes = {
     blue: '#00e5ff'
   }
 }
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Dancing+Script:400,700|Glegoo:400,700|Lustria:400,700|Stint+Ultra+Condensed:400,700|Work+Sans:400,700|Metrophobic:400,700|Taviraj:400,700');
+  
+  body {
+    margin: 0;
+    color: hsla(0, 0%, 0%, 0.8);
+  }
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+  }
+`
