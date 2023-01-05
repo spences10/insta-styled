fc5f0a04e3cec0257ef3f6a0aff045c173a5d574
spences10 <spences10apps@gmail.com>
2023-01-05 20:32:56 +0000

    feat: :sparkles: use filters on index

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Filters from '$lib/components/filters.svelte';
+	import '../instagram.css';
+
+	let optionValue = '';
+</script>
+
+<figure class={`filter-${optionValue}`}>
+	<img src="https://picsum.photos/200/300" alt="" />
+</figure>
+
+<Filters bind:value={optionValue} />
